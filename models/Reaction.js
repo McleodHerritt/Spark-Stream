7ba61414c1256cce448f0123214f48670ce0d25e
McleodHerritt <nmcleod2021@gmail.com>
2023-11-24 15:32:01 -0400

    added reaction and thoughts models

---
+++ models/Reaction.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const reactionSchema = new Schema({
+  reactionId: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: () => new mongoose.Types.ObjectId(),
+  },
+  reactionBody: {
+    type: String,
+    required: true,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (createdAtVal) => new Date(createdAtVal).toLocaleString(),
+  },
+});
+
+module.exports = reactionSchema;
--- models/User.js
+++ models/User.js
@@ -1,37 +1,36 @@
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require("mongoose");
 
-const userSchema = new Schema(
+const userSchema = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+  },
+  thoughts: [
     {
-        username: {
-            type: String,
-            unique:true,
-            required: true,
-            trim: true
-        }
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            match: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
-          },
-          thoughts: [
-            {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: 'Thought',
-            },
-          ],
-          friends: [
-            {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: 'User',
-            },
-          ],
-        });
-        
-        userSchema.virtual('friendCount').get(function() {
-            return this.friends.length;
-          });
-          
-        const User = mongoose.model('User', userSchema);
-        
-        module.exports = User;
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Thought",
+    },
+  ],
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
